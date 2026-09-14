ada861c4ad2f2d50eb1ff7223605ec77e99ab4cc
Денис Берёзкин <Tyuiu.BerezkinDA@outlook.com>
2026-09-14 19:35:35 +0300

    Добавьте файлы проекта.

---
+++ Tyuiu.BerezkinDA.Sprint1.Task0.V2.Lib/DataService.cs
@@ -0,0 +1,10 @@
+﻿namespace Tyuiu.BerezkinDA.Sprint1.Task0.V2.Lib
+{
+    public class DataService
+    {
+        public static int DoSomething()
+        {
+            return (3 / 3 * 2 + 12 / 4 / 3 - 2);
+        }
+    }
+}
---
+++ Tyuiu.BerezkinDA.Sprint1.Task0.V2.Test/DataServiceTest.cs
@@ -0,0 +1,11 @@
+﻿namespace Tyuiu.BerezkinDA.Sprint1.Task0.V2.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
